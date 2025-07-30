83bbd8548e5ce0d5ce48f1a46c113e3c8256e450
Lou Chevrollier <77065045+lcvl41@users.noreply.github.com>
2025-07-30 11:29:52 +0200

    vectorize func mixing column ops and adapt tests

--- snicarfx/rt_solvers/__init__.py
+++ snicarfx/rt_solvers/__init__.py
@@ -1,9 +1,9 @@
-"""Radiative transfer solvers for biosnicar.
+"""Radiative transfer solvers for snicar-fx.
 
 This module contains implementations of the radiative transfer solver:
 - Adding-doubling solver
 """
 
-from biosnicar.rt_solvers.adding_doubling_solver import adding_doubling_solver
+from snicarfx.rt_solvers.adding_doubling_solver import adding_doubling_solver
 
 __all__ = [ "adding_doubling_solver"] 
