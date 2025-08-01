92ffa37ce9a904d3adba6431d4252981487434ec
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2025-08-01 17:52:41 +0200

    change module structure

--- snicarfx/classes/__init__.py
+++
@@ -1,9 +0,0 @@
-from .column_properties import ColumnProperties
-from .model_inputs import ModelInputs
-from .solar_irradiance import SolarIrradiance
-
-__all__ = [
-    'ColumnProperties',
-    'ModelInputs',
-    'SolarIrradiance',
-] 
