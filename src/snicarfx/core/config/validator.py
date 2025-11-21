5158c4eac1669696de27bded2c6305ef5c8b0565
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2025-11-21 21:50:29 +0100

    adapt validator to match new input file structure

--- src/snicarfx/core/config/validator.py
+++ src/snicarfx/core/config/validator.py
@@ -5,24 +5,23 @@ https://github.com/openosmia/snicar-fx
 
 """
 
-from typing import Literal
+from typing import Dict, Literal
 
 import yaml
-from pydantic import BaseModel, Field, confloat, conlist, model_validator
+from pydantic import BaseModel, RootModel, Field, confloat, conlist, model_validator
 
 
-class Rtm(BaseModel):
+class Solver(BaseModel):
     """
-    Define the valid ranges and types of the radiative transfer model (RTM)
-    configuration.
 
-    Inherits from the Pydantic BaseModel class, which enables automatic type 
+    Define the valid ranges and types of the solver used in the configuration.
+
+    Inherits from the Pydantic BaseModel class, which enables automatic type
     validation and parsing of yaml files.
-        
-    """ 
-        
-    # radiation (0 is direct 1 is diffuse)
-    DIRECT: Literal[0, 1]
+    """
+
+    # radiative transfer solver to use
+    TYPE: Literal["two-stream", "multi-stream"]
 
     # first wavelength (unit: nm)
     WVL_START: int = Field(..., ge=200, le=5000)
@@ -33,50 +32,43 @@ class Rtm(BaseModel):
     # spectral resolution (unit: nm)
     RESOLUTION: int = Field(..., ge=1, le=100)
 
-    # Solar Zenith Angle (unit: degrees)
-    SZA: int = Field(..., ge=0, le=89)
-
-    # type of irradiance
-    IRRADIANCE_TYPE: Literal["mls", "mlw", "saw", "sas", "smm", "hmn", "trp"]
+    # explicit surface-atmosphere coupling
+    ATMOSPHERE_COUPLING: bool
 
     # only fields validated here are allowed
     model_config = {"extra": "forbid"}
 
 
-class Ice(BaseModel):
+class Solar(BaseModel):
     """
-    Define the valid ranges and types of the ice/snow properties.
 
-    Inherits from the Pydantic BaseModel class, which enables automatic type 
+    Define the valid ranges and types of the solar properties.
+
+    Inherits from the Pydantic BaseModel class, which enables automatic type
     validation and parsing of yaml files.
-        
-    """ 
+    """
 
-    # thickness of each vertical layer (unit : m)
-    THICKNESS: conlist(confloat(ge=1e-10, le=1000), min_length=1, max_length=1000)
+    # Solar Zenith Angle (unit: degrees)
+    SZA: int = Field(..., ge=0, le=89)
 
-    # 0: ice spheres, 1: solid ice w/frsnl, 2: w/out frsnl
-    LAYER_TYPE: conlist(Literal[0, 1, 2], min_length=1, max_length=1000)
+    # only fields validated here are allowed
+    model_config = {"extra": "forbid"}
 
-    # density of each layer (unit : kg m-3)
-    DENSITY: conlist(confloat(ge=10, le=925), min_length=1, max_length=1000)
 
-    # source of refraction index
-    RF_TYPE: Literal["Pic16", "Wrn08", "Coop21"]
+class Atmosphere(BaseModel):
+    """
 
-    # m2 kg-1
-    SPECIFIC_SURFACE_AREA: conlist(
-        confloat(ge=1e-10, le=100), min_length=1, max_length=1000
-    )
+    Define the valid ranges and types of the atmosphere properties.
 
-    # LWC content in snow/ice
-    LWC: conlist(confloat(ge=0.0, le=1.0), min_length=1, max_length=1000)
+    Inherits from the Pydantic BaseModel class, which enables automatic type
+    validation and parsing of yaml files.
+    """
 
-    # reflectance of lower boundary
-    SFC: float = Field(..., ge=0.0, le=1.0)
+    # sky conditions to apply direct- or diffuse-dominated radiation
+    SKY_CONDITIONS: Literal["clear", "cloudy"]
 
-    # grain shape: 0 is sphere, 1 is Robledano et al. 2023
-    GRAIN_SHAPE: conlist(Literal[0, 1], min_length=1, max_length=1000)
+    # type of atmospheric profile
+    ATMOSPHERIC_PROFILE_TYPE: Literal["afglss"]
 
     # only fields validated here are allowed
     model_config = {"extra": "forbid"}
@@ -87,87 +79,130 @@ class Particle(BaseModel):
     Define the valid ranges and types for the properties of a light absorbing
     particle.
 
-    Inherits from the Pydantic BaseModel class, which enables automatic type 
+    Inherits from the Pydantic BaseModel class, which enables automatic type
     validation and parsing of yaml files.
-        
-    """ 
-    
-    FILE: str
-    COATED: bool
-    UNIT: Literal[0, 1]
+
+    """
+
+    # check that file name has netCDF or csv extension
+    FILE: str = Field(..., pattern=r".*\.(nc|csv)$")
+
+    # light absorbing particle concentration
     CONC: conlist(confloat(ge=0.0, le=1e10), min_length=1, max_length=1000)
 
     # only fields validated here are allowed
     model_config = {"extra": "forbid"}
 
 
-class LightAbsorbingParticles(BaseModel):
+class LightAbsorbingParticles(RootModel[Dict[str, Particle]]):
     """
     Define an object for the configuration of all light absorbing particles.
+    Ensure each key in LIGHT_ABSORBING_PARTICLES is valid.
 
-    Inherits from the Pydantic BaseModel class, which enables automatic type 
+    Inherits from the Pydantic BaseModel class, which enables automatic type
     validation and parsing of yaml files.
-        
-    """ 
-    
-    # Optional: Black carbon and algae
-    BC: Particle | None = None
-    ALG: Particle | None = None
+
+    """
+
+    pass
+
+
+class Land(BaseModel):
+    """
+    Define the valid ranges and types of the land surface properties.
+
+    Inherits from the Pydantic BaseModel class, which enables automatic type
+    validation and parsing of yaml files.
+
+    """
+
+    # thickness of each vertical layer (unit : m)
+    THICKNESS: conlist(confloat(ge=1e-10, le=1000), min_length=1, max_length=1000)
+
+    # 0: ice spheres, 1: solid ice w/frsnl, 2: w/out frsnl
+    LAYER_TYPE: conlist(Literal[0, 1, 2], min_length=1, max_length=1000)
+
+    # density of each layer (unit : kg m-3)
+    DENSITY: conlist(confloat(ge=10, le=925), min_length=1, max_length=1000)
+
+    # m2 kg-1
+    SPECIFIC_SURFACE_AREA: conlist(
+        confloat(ge=1e-10, le=100), min_length=1, max_length=1000
+    )
+
+    # LWC content in snow/ice
+    LWC: conlist(confloat(ge=0.0, le=1.0), min_length=1, max_length=1000)
+
+    # source of refraction index
+    RF_TYPE: Literal["Pic16", "Wrn08", "Coop21"]
+
+    # reflectance of lower boundary
+    SFC: float = Field(..., ge=0.0, le=1.0)
+
+    # grain shape: 0 is sphere, 1 is Robledano et al. 2023
+    GRAIN_SHAPE: conlist(Literal[0, 1], min_length=1, max_length=1000)
+
+    # surface altitude (km)
+    ALTITUDE: float = Field(..., ge=0.0, le=9.0)
+
+    # all provided light absorbing particles
+    LIGHT_ABSORBING_PARTICLES: LightAbsorbingParticles | None = None
 
     # only fields validated here are allowed
-    # model_config = {"extra": "forbid"}
+    model_config = {"extra": "forbid"}
 
 
 class Config(BaseModel):
     """
     Combine the different objects inheriting from BaseModel.
 
-    Inherits from the Pydantic BaseModel class, which enables automatic type 
+    Inherits from the Pydantic BaseModel class, which enables automatic type
     validation and parsing of yaml files.
-        
-    """ 
-    
-    RTM: Rtm
-    ICE: Ice
-    LIGHT_ABSORBING_PARTICLES: LightAbsorbingParticles
+
+    """
+
+    SOLVER: Solver
+    SOLAR: Solar
+    ATMOSPHERE: Atmosphere
+    LAND: Land
 
     model_config = {"extra": "forbid"}
 
     @model_validator(mode="after")
     def check_lengths(self):
         """
-        Check that all ICE and LIGHT_ABSORBING_PARTICLES
+        Check that all LAND and LIGHT_ABSORBING_PARTICLES
         layer-related lists have the same length
 
         """
 
-        ice_lists = [
-            self.ICE.THICKNESS,
-            self.ICE.LAYER_TYPE,
-            self.ICE.DENSITY,
-            self.ICE.SPECIFIC_SURFACE_AREA,
-            self.ICE.LWC,
-            self.ICE.GRAIN_SHAPE,
+        land_lists = [
+            self.LAND.THICKNESS,
+            self.LAND.LAYER_TYPE,
+            self.LAND.DENSITY,
+            self.LAND.SPECIFIC_SURFACE_AREA,
+            self.LAND.LWC,
+            self.LAND.GRAIN_SHAPE,
         ]
-        lengths = {len(lst) for lst in ice_lists}
+        lengths = {len(lst) for lst in land_lists}
 
         if len(lengths) > 1:
             raise ValueError(
-                f"All ICE layer-related lists must have the same length, but now"
-                f"are: {[len(lst) for lst in ice_lists]}"
+                f"All LAND layer-related lists must have the same length, but now"
+                f"are: {[len(lst) for lst in land_lists]}"
             )
 
-        ice_layers = len(self.ICE.THICKNESS)
+        land_layers = len(self.LAND.THICKNESS)
 
-        # Check that all particle CONC lists match ICE layers
-        if self.LIGHT_ABSORBING_PARTICLES is not None:
-            for particle_name in self.LIGHT_ABSORBING_PARTICLES.model_fields:
-                particle = getattr(self.LIGHT_ABSORBING_PARTICLES, particle_name)
-                if particle is not None and len(particle.CONC) != ice_layers:
+        # Check that all particle CONC lists match LAND layers
+        laps = getattr(self.LAND, "LIGHT_ABSORBING_PARTICLES", None)
+        if laps is not None:
+            for particle_name, particle in laps.root.items():  # <-- use .root
+                if particle is not None and len(particle.CONC) != land_layers:
                     raise ValueError(
-                        f"Particle {particle_name} CONC list length "
-                        f"({len(particle.CONC)}) does not match number of ice "
-                        f"layers ({ice_layers})"
+                        f"Particle '{particle_name}' CONC list length "
+                        f"({len(particle.CONC)}) does not match number of land "
+                        f"layers ({land_layers})"
                     )
 
         return self
