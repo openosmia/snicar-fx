ab8be91dade7663e34c537685f06f2c7bea1a47d
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2026-02-21 12:57:31 +0100

    add back session tests

--- tests/functional/test_session.py
+++ tests/functional/test_session.py
@@ -5,19 +5,42 @@ https://github.com/openosmia/snicar-fx
 
 """
 
-"""
-This file is part of the snicar-fx software package.
-
-https://github.com/openosmia/snicar-fx
-
-"""
-
 import pytest
 import yaml
 from snicarfx.core.session.session import Session
 import tempfile
 import os
 import numpy as np
+from pydantic import BaseModel
+
+
+def test_session_attributes(session):
+    """
+    Verify that the required attributes of Session exist and have the correct
+    types.
+
+    Parameters
+    ----------
+    session : Session
+        Instance of the Session class
+    """
+
+    assert hasattr(session, "config")
+    assert isinstance(session.config, BaseModel)
+
+
+def test_get_package_root(session):
+
+    package_root = session.get_package_root()
+
+    assert "snicar-fx" in package_root.parts
+
+
+# def test_run(session):
+
+#     outputs = session.run()
+
+#     assert isinstance(outputs.BBA, float)
 
 
 @pytest.mark.parametrize(
