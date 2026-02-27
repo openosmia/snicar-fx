97fc85277fbdf770fb505c25644387026fb87aea
Lou Chevrollier <77065045+lcvl41@users.noreply.github.com>
2026-02-27 13:32:40 +0100

    clean docstring solar

--- src/snicarfx/core/components/solar.py
+++ src/snicarfx/core/components/solar.py
@@ -10,29 +10,23 @@ import xarray as xr
 
 class SolarIrradiance:
     """
-    Compute and and store the properties of the incoming solar irradiance.
+    Compute and and store the properties of the incoming solar irradiance to be
+    used as boundary for the solver (top-of-atmosphere or bottom-of-atmosphere).
 
     Attributes
     ----------
-    model_inputs : ModelInputs
-        An instance of the ModelInputs class containing model input data parsed
+    config : Config
+        An instance of the Config class containing model input data parsed
         from the YAML configuration file.
-    direct : boolean
-        If True, use direct radiation; if False, use diffuse radiation.
-    sza : int
-        Solar zenith angle (SZA).
-    atmosphere_type : str
-        The atmopshere profile to use.
     """
 
     def __init__(self, config):
         """
         Initialize the SolarIrradiance class using model configuration inputs.
 
-        This constructor extracts solar irradiance parameters from the given
-        `model_inputs` object, including whether to use direct/diffuse radiation,
-        the solar zenith angle (SZA), and the irradiance type. It then triggers
-        irradiance processing via the `set_irradiance()` method.
+        This constructor extracts parameters from the given
+        `config` object and then triggers irradiance processing via the 
+        `set_irradiance()` method.
 
         Parameters
         ----------
@@ -41,7 +35,6 @@ class SolarIrradiance:
             from the YAML configuration file.
         """
 
-        # set module root path for data loading
         self.ROOT_PATH = config._ROOT_PATH
 
         self.sky_conditions = config.ATMOSPHERE.SKY_CONDITIONS
@@ -50,7 +43,6 @@ class SolarIrradiance:
 
         self.atmosphere_type = config.ATMOSPHERE.ATMOSPHERIC_PROFILE_TYPE
 
-        # set irradiance based on user inputs
         if config.SOLVER.ATMOSPHERE_COUPLING:
             self.irradiance_dataset = self.load_toa_irradiance()
             self.set_toa_irradiance(config)
@@ -61,11 +53,12 @@ class SolarIrradiance:
 
     def load_surface_irradiance(self):
         """
-        Load irradiance file: surface irradiance profiles modelled with
-        LibRadTran for a range of SZAs.
+        Load surface irradiance file.
+        
+        The surface irradiance file stores pre-computed irradiances simulated 
+        with LibRadTran for a given atmospheric profile and range of SZAs.
         """
 
-        # read libradtran surface irradiance
         ds = xr.open_dataset(
             str(
                 f"{self.ROOT_PATH}/data/solar_fluxes/"
@@ -78,8 +71,10 @@ class SolarIrradiance:
 
     def load_toa_irradiance(self):
         """
-        Load TOA irradiance profile: Version 2 of the TSIS-1 Hybrid Solar
-        Reference Spectrum from Coddington et al. 2022.
+        Load top-of-atmosphere (TOA) irradiance file.
+        
+        The TOA irradiance file corresponds Version 2 of the TSIS-1 Hybrid 
+        Solar Reference Spectrum from Coddington et al. 2022.
         """
 
         ds = xr.open_dataset(
@@ -93,105 +88,57 @@ class SolarIrradiance:
 
     def set_toa_irradiance(self, config):
         """
-        Compute solar spectral irradiance at top-of-atmosphere (TOA) on the
-        user-defined wavelength grid / satellite bands.
+        Set monochromatic top-of-atmosphere (TOA) solar spectral irradiance 
+        array used as boundary for the solver.
+        
+        This method interpolates the TOA spectral irradiance to the solar
+        wavelength array. 
+        
+        The following instance attributes are set:
+        - `flx_slr` : ndarray
+            Total spectral solar irradiance at TOA.
         """
 
         self.irradiance_dataset["wavelength"] = self.irradiance_dataset[
             "Vacuum Wavelength"
         ]
 
-        # if (
-        #     config.SPECTRAL.MODE == "monochromatic"
-        #     or config.SPECTRAL.BAND_METHOD == "srf-integration"
-        # ):
         self.flx_slr = self.irradiance_dataset.interp(
             wavelength=config._wavelengths_solar
-        ).SSI.values  # W m-2 nm-1
-
-        # elif config.SPECTRAL.BAND_METHOD == "snicar-default":
-        #     self.flx_slr = compute_band_average(
-        #         self.irradiance_dataset.SSI,
-        #         config._band_ranges,
-        #         wavelength_dim="wavelength",
-        #     )
-
-        # elif config.SOLVER.SPECTRAL_MODE == "band":
-        #     self.flx_slr = compute_bin_average(
-        #         self.irradiance_dataset.SSI, config._wavelengths
-        #     )
-
-        # elif config.SOLVER.SPECTRAL_MODE == "band":
-
-        #     # interpolate SENTINEL-3-OLCI SRF on TOA irradiance wavelengths
-        #     srf_on_toa_grid = np.vstack(
-        #         [
-        #             np.interp(
-        #                 self.irradiance_dataset["Vacuum Wavelength"].values,
-        #                 config._wavelengths_srf[band_number, :],
-        #                 config._spectral_response_function[band_number, :],
-        #             )
-        #             for band_number in range(21)
-        #         ]
-        #     )
-
-        #     toa_irradiance = self.irradiance_dataset.SSI.values
-
-        #     # collapse TOA irradiance on S3 bands
-        #     self.flx_slr = np.nansum(
-        #         (srf_on_toa_grid * toa_irradiance[None, :]), axis=1
-        #     ) / (np.nansum(srf_on_toa_grid, axis=1))
+        ).SSI.values  
 
         return None
 
     def set_surface_irradiance(self, config):
         """
-        Compute the solar spectral irradiance at the surface.
+        Set monochromatic surface solar spectral irradiance array used as 
+        boundary for the solver.
 
-        This method interpolates the solar flux to the spectral resolution
-        defined in the input file and loads the direct (`fs`) and
-        diffuse (`fd`) fluxes.
+        This method selects the irradiance corresponding to the user-input 
+        solar zenith angle (SZA) and interpolates the spectral irradiance to 
+        the solar wavelength array. 
 
         The following instance attributes are set:
         - `flx_slr` : ndarray
-            Normalized total spectral solar flux at the surface.
+            Total (diffuse + direct) spectral solar irradiance at the surface.
         - `fs` : ndarray
-            Normalized spectral direct irradiance at the surface.
+            Spectral direct irradiance at the surface.
         - `fd` : ndarray
-            Normalized spectral diffuse irradiance at the surface.
+            Spectral diffuse irradiance at the surface.
         """
 
-        # index irradiance dataset on given SZA
+
         ds_sza = self.irradiance_dataset.sel(SZA=self.sza)
 
-        # if (
-        #     config.SPECTRAL.MODE == "monochromatic"
-        #     or config.SPECTRAL.BAND_METHOD == "srf-integration"
-        # ):
         ds_sza = ds_sza.interp(wavelength=config._wavelengths_solar,
                                kwargs={"fill_value": "extrapolate"})
-
-        # elif config.SPECTRAL.BAND_METHOD == "snicar-default":
-        #     ds_sza = compute_band_average(
-        #         ds_sza,
-        #         config._band_ranges,
-        #         wavelength_dim="wavelength",
-        #     )
 
         irradiance_direct = ds_sza.sel(irradiance_type="direct")["irradiance"]
         irradiance_diffuse = ds_sza.sel(irradiance_type="diffuse")["irradiance"]
 
-        # sum over wavelengths to get total
-        irradiance_total_sum = (irradiance_direct + irradiance_diffuse).sum(
-            dim="wavelength"
-        )
-
-        irradiance_direct_normalized = irradiance_direct #/ irradiance_total_sum
-        irradiance_diffuse_normalized = irradiance_diffuse #/ irradiance_total_sum
-
         # replace 0s by 1e-30 to avoid invalid operations
-        self.fs = irradiance_direct_normalized.clip(min=1e-30).values
-        self.fd = irradiance_diffuse_normalized.clip(min=1e-30).values
+        self.fs = irradiance_direct.clip(min=1e-30).values
+        self.fd = irradiance_diffuse.clip(min=1e-30).values
 
         # solar flux is direct + diffuse
         self.flx_slr = self.fs + self.fd
