f3b9dfce7c4b407dbc3946899f933d8f1bd38a5b
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2026-06-14 13:59:36 +0200

    force single-threaded BLAS/OpenMP execution

--- src/snicarfx/__init__.py
+++ src/snicarfx/__init__.py
@@ -1,3 +1,13 @@
+import os
+
+# force single-threaded BLAS/OpenMP execution (to prevent e.g. core
+# oversubscription in HPC environments)
+os.environ["OMP_NUM_THREADS"] = "1"
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["OPENBLAS_NUM_THREADS"] = "1"
+os.environ["VECLIB_MAXIMUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+
 from importlib.metadata import version
 
 from .core.session.config import Config
